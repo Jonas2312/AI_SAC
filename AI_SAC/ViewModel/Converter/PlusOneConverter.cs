20a84216b0390bb6cf314ab99745f50255120270
Ile de France <Ile de France>
2020-03-26 18:56:22 +0100

    AI_SAC: Automatical commit (Date: 18:56:22,24)

--- AI_SAC/ViewModel/Converter/PlusOneConverter.cs
+++ AI_SAC/ViewModel/Converter/PlusOneConverter.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace AI_SAC.ViewModel.Converter
 {
     class PlusOneConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return (value as int) + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as int) - 1;
+        }
     }
 }
