c83c570182559a6fede4b71805551b4a1e9ff215
Ile de France <Ile de France>
2020-03-26 19:06:30 +0100

    AI_SAC: Automatical commit (Date: 19:06:30,18)

--- AI_SAC/View/EditorView.xaml.cs
+++ AI_SAC/View/EditorView.xaml.cs
@@ -42,6 +42,22 @@ namespace AI_SAC.View
             LoadDatabase.Visibility = Visibility.Visible;
         }
 
+        private void OnSearchTextChanged(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < dgTest.Rows.Count; i++)
+            {
+                if (dgTest.Rows[i].Cells[0].Value.ToString() == "search")
+                {
+                    dgTest.Rows[i].Selected = true;
+                    dgTest.Rows[i].Visible = true;
+                }
+                else
+                {
+                    dgTest.Rows[i].Visible = false;
+                    dgTest.Rows[i].Selected = false;
+                }
+            }
+        }
         private void StartProgramButton_Click(object sender, RoutedEventArgs e)
         {
             ProgramRunningText.Visibility = Visibility.Visible;
