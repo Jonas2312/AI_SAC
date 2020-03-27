b67812b96f79c8f6e0d0c7cc1a3fc39c6b344e5f
Ile de France <Ile de France>
2020-03-27 20:30:48 +0100

    AI_SAC: Automatical commit (Date: 20:30:48,54)

---
+++ AI_SAC/Model/Analyzer/AI_Analyzer.cs
@@ -0,0 +1,22 @@
+﻿using AI_SAC.Model.HookFeed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_SAC.Model.Analyzer
+{
+    public class AI_Analyzer : KeyAnalyzer
+    {
+        public override void checkInputs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RecieveKeyPress(KeyData key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AI_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AI_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AI_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84264a22-0c67-4385-ae16-40b563235db8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
